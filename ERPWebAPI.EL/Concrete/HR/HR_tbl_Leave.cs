00eec50c615db281d14e34c7fac3ba448597e979
FishTuna <fishexy@gmail.com>
2024-09-12 11:58:28 +0300

    HR LEAVE ENTİTY REFACTOR

--- ERPWebAPI.EL/Concrete/HR/HR_tbl_Leave.cs
+++ ERPWebAPI.EL/Concrete/HR/HR_tbl_Leave.cs
@@ -9,9 +9,15 @@ namespace ERPWebAPI.EL.Concrete.HR
         [Key]
         public int LEAVEID { get; set; }
         public int EMPLOYEEID { get; set; }
+        public string? EMPLOYEE_NAME { get; set; }
+        public string? EMPLOYEE_LASTNAME { get; set; }
+        public string? REGISTRY_NO { get; set; }
         public int FORMID { get; set; }
-        public bool IS_ACTIVE { get; set; }
         public byte VACATIONID { get; set; }
+        public string? VACATION_NAME { get; set; }
+        public byte VACATION_GROUP_ID { get; set; }
+        public string? VACATION_GROUP_NAME { get; set; }
+        public byte VACATION_GROUP_PRIORITY { get; set; }
         public bool ISPAID { get; set; }
         public bool ISHOURLY { get; set; }
         public bool UNAFFECT_SENIORITY { get; set; }
@@ -23,13 +29,15 @@ namespace ERPWebAPI.EL.Concrete.HR
         public DateTime ONWORK_DATE { get; set; }
         public float RAW_DURATION { get; set; }
         public float DAYOFF_COUNT { get; set; }
-        public float HOLIDAY_COUNT { get; set;}
+        public float HOLIDAY_COUNT { get; set; }
         public float NET_DURATION { get; set; }
         public byte DATEPRIOD_TYPE_ID { get; set; }
+        public string? DATEPERIOD_TYPE { get; set; }
         public string? COMMENT { get; set; }
         public string? VACATION_ADRESS { get; set; }
         public DateTime TRANSACTION_DATE { get; set; }
         public int USER_EMPLOYEE_ID { get; set; }
-        public string LOGINNAME { get; set; }
+        public string? LOGIN_NAME { get; set; }
+
     }
 }
