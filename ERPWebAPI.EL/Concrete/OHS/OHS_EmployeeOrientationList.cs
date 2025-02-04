62e9eacc3c6799fe3e1850cc3b68df1f34b57fb9
FishTuna <fishexy@gmail.com>
2025-02-04 19:13:01 +0300

    mmpi

--- ERPWebAPI.EL/Concrete/OHS/OHS_EmployeeOrientationList.cs
+++ ERPWebAPI.EL/Concrete/OHS/OHS_EmployeeOrientationList.cs
@@ -7,19 +7,24 @@ namespace ERPWebAPI.EL.Concrete.OHS
     {
         [Key]
         public int Id { get; set; }
-        public bool IsPlanned { get; set; }
-        public string NameLastName { get; set; }
-        public short LocationId { get; set; }
-        public string Location { get; set; }
+        public int EmployeeId { get; set; }
+        public string? NameLastName { get; set; }
+        public DateTime? EmploymentdDate { get; set; }
+        public short FacilityId { get; set; }
+        public string? Facility { get; set; }
         public short DepartmentId { get; set; }
-        public string Department { get; set; }
+        public string? Department { get; set; }
+        public short PositionId { get; set; }
+        public string? Position { get; set; }
         public short JobId { get; set; }
-        public string JobName { get; set; }
-        public short OrientationTypeId { get; set; }
-        public string OrientationType { get; set; }
-        public DateTime EndDate { get; set; }
-        public string UserName { get; set; }
-        public int UserEmployeeId { get; set; }
-        public DateTime TransactionDate { get; set; }
+        public string? JobName { get; set; }
+        public bool IsPlanned { get; set; }
+        public int PlanId { get; set; }
+        public DateTime? PlanDate { get; set; }
+        public short ReasonId { get; set; }
+        public string? Reason { get; set; }
+        public DateTime? OrientationDate { get; set; }
+        public string? UserEmployee { get; set; }
+        public DateTime? TransactionDate { get; set; }
     }
 }
